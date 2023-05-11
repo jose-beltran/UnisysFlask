bf839825dd7b168c22c7e7f2db6c0de29c362548
Jose Beltran <55297825+jose-beltran@users.noreply.github.com>
2023-05-10 23:33:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,6 @@
 from flask import Flask, render_template, request, url_for, redirect
-import mysql.connector
 
 app = Flask(__name__)
-
-# Configurar la conexión a la base de datos
-db = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="12345678",
-    database="unisys"
-)
-
 
 @app.route('/')
 def index():
@@ -25,23 +15,9 @@ def documentar():
 
 
 @app.route('/seguimientos', methods=['GET', 'POST'])
-def historial():
+def seguimiento():
     data = {'titulo': 'Seguimientos'}
-    # Consulta para obtener todas las plantillas de la tabla "plantillas_usd"
-    cursor = db.cursor()
-    cursor.execute("SELECT Nombre FROM plantillas_usd")
-    plantillas = cursor.fetchall()
-
-    if request.method == 'POST':
-        plantilla = request.form['plantilla']
-        cursor.execute(f"SELECT Contenido FROM plantillas_usd WHERE Nombre='{plantilla}'")
-        contenido = cursor.fetchone()[0]
-        copiado = request.form['contenido']
-    else:
-        contenido = ""
-
-    return render_template('seguimiento.html', plantillas=plantillas, contenido=contenido, data=data)
-
+    return render_template('seguimiento.html', data=data)
 
 @app.route('/plantillas')
 def plantillas():
