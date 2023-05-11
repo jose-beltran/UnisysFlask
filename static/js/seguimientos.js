56ddebe977126298dc16dd17e86815656e29e91d
Jose Beltran <55297825+jose-beltran@users.noreply.github.com>
2023-05-10 23:29:09 -0500

    Add files via upload

--- static/js/seguimientos.js
+++ static/js/seguimientos.js
@@ -1,12 +1,22 @@
-function mostrarDatos() {
-    var nombre = document.getElementById("nombre").value;
-    var incidente = document.getElementById("incidente").value;
-    var time = document.getElementById("time").value;
-    var date = document.getElementById("date").value;
-    
-    var texto = "Buenos dias "+ nombre + ', como esta?' +'\n'+ 
-        'Te escribo desde MATEO por el incidente'+ incidente;
-    
+var botonGenerar = document.getElementById("boton")
+var nombre = document.getElementById("nombre");
+var incidente = document.getElementById("incidente");
+
+function mostrarBoton() {
+    if (nombre.value !== "" && incidente.value !== "") {
+        botonGenerar.style.display = "inline-block";
+    } else {
+        botonGenerar.style.display = "none";
+    }
+}
+
+function mostrar() {
+    nom = nombre.value;
+    inc = incidente.value;
+    var texto = "Buenos días " + nom + ', como esta?\nLe escribo desde MATEO por el incidente ' + inc + "";
     document.getElementById("texto").innerHTML = texto;
-  }
-  
+}
+
+botonGenerar.addEventListener("click", mostrar);
+nombre.addEventListener("input", mostrarBoton);
+incidente.addEventListener("input", mostrarBoton);
